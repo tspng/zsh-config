c6d02674e7e139e60ff72725213cc1037be1099a
tspng <tspng@socket.ch>
2015-12-12 11:09:02 +0100

    Added some aliases.

--- aliases.zsh
+++ aliases.zsh
@@ -1,5 +1,11 @@
+# General
 alias psgrep="ps wwaux | grep"
 alias rm_pyc="find . -name \*\.pyc -exec rm {} \;"
-alias mpupdate="sudo port selfupdate && sudo port upgrade outdated && sudo port uninstall inactive"
+
+# Home
 alias wakup_tuborg="wol 00:E0:18:E5:82:AC"
 alias wakup_mediamac="wol 00:26:4A:08:F1:88"
+
+# Git
+alias gst="git status"
+alias gd="git diff"
