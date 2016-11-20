192eb7383bac01299742029d8c612381b37aa798
tspng <tspng@socket.ch>
2016-11-20 12:54:52 +0100

    added some docker aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,10 +1,14 @@
 # General
-alias psgrep="ps wwaux|grep"
+alias psgrep='ps wwaux|grep'
 
 # Mac specific
-alias bs="brew services"
-alias clipkey="cat ~/.ssh/id_rsa.pub|pbcopy"
+alias bs='brew services'
+alias clipkey='cat ~/.ssh/id_rsa.pub|pbcopy'
 
 # Development stuff
 alias activate='source `find . -name activate -type f|head -1)`'
-alias rm_pyc="find . -name \*\.pyc -exec rm {} \;"
+alias rm_pyc='find . -name \*\.pyc -exec rm {} \;'
+
+# Docker
+alias dshell_lastest='docker exec -i -t $(docker ps -n1 --format "{{.ID}}") bash -l'
+alias denv='eval $(docker-machine env default)'
