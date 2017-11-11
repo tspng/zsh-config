6940da5476188a612cddf08055ca03597f6e050a
tspng <tspng@socket.ch>
2017-11-11 14:42:42 +0100

    Change environment to use pyenv

--- env.zsh
+++ env.zsh
@@ -1,18 +1,12 @@
-# Path
-export PATH=~/bin:/usr/local/bin:/usr/local/sbin:/usr/local/opt/python/libexec/bin:$PATH
-export MANPATH=/usr/local/share/man:$MANPATH
-
 # Lanugage settings
 export LANG='de_CH.UTF-8'
 export LC_CTYPE='en_US.UTF-8'
 # CTAGS Sorting in VIM/Emacs is better behaved with this in place
 export LC_COLLATE=C
 
-
 # Enable color in grep
 export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='3;33'
-
 
 if [[ "$OSTYPE" == darwin* ]]; then
   export BROWSER='open'
@@ -25,6 +19,8 @@ export LESSCHARSET='utf-8'
 # Java
 JAVA_HOME=`/usr/libexec/java_home`
 
-# Python virtualenv settings
-export WORKON_HOME=$HOME/.virtualenvs
-source /usr/local/bin/virtualenvwrapper_lazy.sh
+# Python
+# needs pyenv and pyenv-virtualenvwrapper installed
+export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
+eval "$(pyenv init -)"
+pyenv virtualenvwrapper_lazy
