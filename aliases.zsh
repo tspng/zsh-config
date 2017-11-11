c47f5e9b5ef4172eb76a60c3e721b16621aa5a6e
tspng <tspng@socket.ch>
2017-11-11 15:02:57 +0100

    Fix typo in alias name

--- aliases.zsh
+++ aliases.zsh
@@ -10,5 +10,5 @@ alias activate='source `find . -name activate -type f|head -1)`'
 alias rm_pyc='find . -name \*\.pyc -exec rm {} \;'
 
 # Docker
-alias dshell_lastest='docker exec -i -t $(docker ps -n1 --format "{{.ID}}") bash -l'
+alias dshell_latest='docker exec -i -t $(docker ps -n1 --format "{{.ID}}") bash -l'
 alias denv='eval $(docker-machine env default)'
