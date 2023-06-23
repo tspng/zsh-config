45b070676a7dfe65c0c4c298697a471db649c76d
tspng <tspng@socket.ch>
2023-06-23 15:04:06 +0200

    Update new fzf initialization

--- env.zsh
+++ env.zsh
@@ -73,7 +73,5 @@ fi
 
 # Fzf Ctrl-R replacement
 if (( $+commands[fzf] )); then
-    # Auto-completion
-    [[ $- == *i* ]] && source "${HOMEBREW_PREFIX}/opt/fzf/shell/completion.zsh"  # 2> /dev/null
-    source "${HOMEBREW_PREFIX}/opt/fzf/shell/key-bindings.zsh"
+    [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 fi
